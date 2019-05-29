9782ad83be27de7a2d3a2944ed08cef474459828
Reckful <626431528@qq.com>
2019-05-29 22:36:08 +0800

    Add event demo.

---
+++ src/chapter_07/die_please.erl
@@ -0,0 +1,45 @@
+-module(die_please).
+
+-behavior(gen_server).
+
+-export([
+  start_link/0
+]).
+
+%% API
+-export([
+  init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3
+]).
+
+-define(SERVER, ?MODULE).
+-define(SLEEP_TIME, (2 * 1000)).
+
+-record(state, {}).
+
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+  {ok, #state{}, ?SLEEP_TIME}.
+
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(timeout, State) ->
+  i_want_to_die = right_now,
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
